17b3029e5d7373b2cb49c87d2a730fb77253c5cb
Jai Pandey <jaipandeyzerozero@gmail.com>
2026-02-08 14:26:22 +0530

    Fetch portfolio images from repo

--- crud-app/backend/app/main.py
+++ crud-app/backend/app/main.py
@@ -4,6 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import select
 from .database import create_db_and_tables, get_session
 from .models import Item, ItemCreate, ItemUpdate
+ 
 
 app = FastAPI(title="FastAPI CRUD Starter")
 
@@ -17,46 +18,50 @@ app.add_middleware(
 
 
 @app.on_event("startup")
-def on_startup() -> None:
-    create_db_and_tables()
-
+def on_startup()-> None:
+    create_db_and_tables() 
 
 def get_db():
     session = get_session()
     try:
-        yield session
+        yield session 
     finally:
-        session.close()
+        session.close() 
 
 
-@app.get("/", tags=["Root"])
+#for basic health check 
+@app.get('/', tags=["Root"])
 def read_root():
-    return {"message": "FastAPI CRUD is running"}
+    return {"message" : "CRUD app started!"}
 
 
-@app.post("/items", response_model=Item, status_code=status.HTTP_201_CREATED)
-def create_item(payload: ItemCreate, session=Depends(get_db)):
-    item = Item.model_validate(payload)
-    session.add(item)
-    session.commit()
-    session.refresh(item)
-    return item
+#creating an item and updating it in the database 
+@app.post("/items", response_model=Item, status_code=status.HTTP_201_CREATED) 
+def create_item(payload:ItemCreate, session=Depends(get_db)):
+     item = Item.model_validate(payload)
+     session.add(item)
+     session.commit()
+     session.refresh(item)
+     return item
 
 
+#returing list of items 
 @app.get("/items", response_model=List[Item])
 def list_items(session=Depends(get_db)):
     items = session.exec(select(Item)).all()
     return items
 
 
+#return one item 
 @app.get("/items/{item_id}", response_model=Item)
-def get_item(item_id: int, session=Depends(get_db)):
-    item = session.get(Item, item_id)
+def get_item(item_id:int, session=Depends(get_db)):
+    item = session.get(Item,item_id)
     if not item:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return item
+        raise HTTPException(status_code=404, detail="item not found!")
+    return item 
 
 
+#update item
 @app.put("/items/{item_id}", response_model=Item)
 def update_item(item_id: int, payload: ItemUpdate, session=Depends(get_db)):
     item = session.get(Item, item_id)
@@ -70,9 +75,10 @@ def update_item(item_id: int, payload: ItemUpdate, session=Depends(get_db)):
     session.add(item)
     session.commit()
     session.refresh(item)
-    return item
+    return item 
 
 
+#delete item 
 @app.delete("/items/{item_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_item(item_id: int, session=Depends(get_db)):
     item = session.get(Item, item_id)
@@ -81,3 +87,6 @@ def delete_item(item_id: int, session=Depends(get_db)):
     session.delete(item)
     session.commit()
     return None
+
+
+
